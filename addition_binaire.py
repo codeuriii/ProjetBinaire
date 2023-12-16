0c0173ffc0afd506456c992d628eba5daa5b3103
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2023-12-16 16:53:39 +0400

    Add file via git

--- addition_binaire.py
+++ addition_binaire.py
@@ -6,6 +6,6 @@ def addition_binaire(liste_binaire1, liste_binaire2):
     dec2 = binaire_vers_decimal(liste_binaire2)
 
     result_dec = dec1 + dec2
-    result_bin = binaire_vers_decimal(result_dec)
+    result_bin = decimal_vers_binaire(result_dec)
 
     return result_bin
