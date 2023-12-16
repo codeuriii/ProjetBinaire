2e0c609fa28d76b1c03d2f7375c97ffe15932646
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2023-12-16 15:21:58 +0400

    update file

--- entier.py
+++ entier.py
@@ -16,3 +16,21 @@ def afficher_binaire(E):
 
 L = [0,0,1,1,1,0,1,0,1]
 print(afficher_binaire(L))
+
+def binaire_vers_decimal(liste_binaire):
+    # Vérifier si la liste est vide
+    if not liste_binaire:
+        return None
+    
+    # Utiliser la fonction join pour concaténer les chiffres binaires en une chaîne
+    chaine_binaire = ''.join(map(str, liste_binaire))
+    
+    # Utiliser la fonction int() pour convertir la chaîne binaire en décimal
+    nombre_decimal = int(chaine_binaire, 2)
+    
+    return nombre_decimal
+
+# Exemple d'utilisation :
+liste_binaire = [1, 0, 1, 0]  # Remplacez cela par votre propre liste
+resultat_decimal = binaire_vers_decimal(liste_binaire)
+print(f"Le nombre décimal correspondant est : {resultat_decimal}")
